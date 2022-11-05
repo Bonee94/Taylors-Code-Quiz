46cbdfbdded425784170d9653c34d45dde6670db
Bonee94 <bonee94@yahoo.com>
2022-11-05 17:12:45 -0500

    Added js display attribute to change visibility of start card and game card

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var questionsScreen = document.getElementById('questions-screen');
+var startingScreen = document.getElementById('starting-screen');
+
+
+questionsScreen.style.display = "none";
+//startingScreen.style.display = "none";
+//questionsCard.style.display = "flex";
+
