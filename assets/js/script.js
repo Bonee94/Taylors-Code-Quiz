fc4ada407eec4f4e51807466d8ab4d703a5372da
Bonee94 <bonee94@yahoo.com>
2022-11-05 21:49:43 -0500

    Added css styling to buttons and Question display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var questionsScreen = document.getElementById('questions-screen');
 var startingScreen = document.getElementById('starting-screen');
+var startingButton = document.getElementById('start-button')
 
 
 questionsScreen.style.display = "none";
@@ -36,18 +37,22 @@ const problemsArray = [
         question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
     },
 ]
-console.log(problemsArray)
+console.log('Problems Array:', problemsArray)
 
 var answersMapped = problemsArray.map(obj => obj.answer)
 var optionsMapped = problemsArray.map(obj => obj.options)
-console.log(answersMapped[3])
-console.log(optionsMapped[4])
+var questionMapped = problemsArray.map(obj => obj.question)
+
+console.log("Q:", questionMapped[3])
+console.log("A:", answersMapped[3])
+console.log("Options:", optionsMapped[3])
 
 function startGame() {
 
     startingScreen.style.display = "none";
     questionsScreen.style.display = "flex";
 
+    writeQuestion()
     writeOptions();
 
 
@@ -55,26 +60,35 @@ function startGame() {
 
 
 }
-document.addEventListener("click", startGame);
+startingButton.addEventListener("click", startGame);
 
+
+function writeQuestion() {
+    var questionHeader = document.getElementById('questions')
+    var currentQuestion = questionMapped[0]
+    questionHeader.append(currentQuestion)
+}
 
 
 function writeOptions() {
     console.log('Wrote Options')
     var olEl = document.getElementById("selectable-answers");
-    var questionSpot = "";
 
-//    Need to be able to change this array number depending on question
+    //    Need to be able to change this array number depending on question
     var arrayToUse = optionsMapped[0];
-    
+
     console.log(optionsMapped[0][1])
     console.log(arrayToUse)
 
-        for (let i = 0; i < arrayToUse.length; i++) {
-            var liEL = document.createElement('li',);
-            liEL.textContent = arrayToUse[i];
-            olEl.append(liEL);
+    for (let i = 0; i < arrayToUse.length; i++) {
+        var liEL = document.createElement('li');
+        var buttonOptions = document.createElement('button');
+        buttonOptions.textContent = arrayToUse[i];
+        liEL.appendChild(buttonOptions)
+        olEl.append(liEL);
 
-            console.log("for loop");
-        }
+        console.log("for loop");
+    }
+
+    return
 };
